822969ac7b2b59814a6b74d64e8c99e05cbea96f
pmahfuz <pedroom@al.insper.edu.br>
2023-05-26 21:09:51 -0300

    ELB com ambiente funcionando

--- terraform/ELB.tf
+++ terraform/ELB.tf
@@ -1,29 +1,31 @@
 resource "aws_elb" "web_elb" {
-  name = "web-elb"  # The name of the Elastic Load Balancer (ELB)
-
+  name = "web-elb"  # Name of the load balancer
   security_groups = [
-    aws_security_group.ec2_security_group.id
-  ]  # The security group(s) associated with the ELB
-  
+    aws_security_group.ec2_security_group.id  # Security group ID(s) associated with the load balancer
+  ]
   subnets = [
-    aws_default_subnet.default_az1.id,
-    aws_default_subnet.second_az1.id
-  ]  # The subnets where the ELB will be deployed
-  
-  cross_zone_load_balancing = true  # Enable cross-zone load balancing
-  
+    aws_subnet.public_subnet.id  # Subnet ID(s) where the load balancer should be created
+  ]
+  instances = [
+    aws_instance.ec2_instance[0].id,  # Instance ID(s) to be registered with the load balancer
+    aws_instance.ec2_instance[1].id
+  ]
+  cross_zone_load_balancing   = true
+
+  # Health check configuration
   health_check {
-    healthy_threshold = 2  # Number of consecutive successful health checks for an instance to be considered healthy
-    unhealthy_threshold = 2  # Number of consecutive failed health checks for an instance to be considered unhealthy
-    timeout = 3  # Number of seconds before considering a health check request as timed out
-    interval = 30  # Interval between health checks in seconds
-    target = "HTTP:80/"  # The target for the health checks (HTTP on port 80)
-  }  # Configuration for health checks
-  
+    healthy_threshold   = 2  # Number of consecutive successful health checks required to consider an instance healthy
+    unhealthy_threshold = 2  # Number of consecutive failed health checks required to consider an instance unhealthy
+    timeout             = 3  # Timeout value for each health check request
+    interval            = 30  # Interval between health checks
+    target              = "HTTP:80/"  # Target for the health check (in this case, HTTP on port 80)
+  }
+
+  # Listener configuration
   listener {
-    lb_port = 80  # The load balancer's port to listen on
-    lb_protocol = "http"  # The protocol for the load balancer (HTTP)
-    instance_port = "80"  # The port on the instances to forward traffic to
-    instance_protocol = "http"  # The protocol for communication with the instances (HTTP)
-  }  # Configuration for the load balancer's listener
+    lb_port           = 80  # Port on the load balancer to listen on
+    lb_protocol       = "http"  # Protocol to use for the load balancer listener
+    instance_port     = 80  # Port on the instances to forward traffic to
+    instance_protocol = "http"  # Protocol to use for communicating with the instances
+  }
 }
--- terraform/ec2.1.tf
+++ terraform/ec2.1.tf
@@ -3,83 +3,102 @@ provider "aws" {
   region    = "us-east-1"
 }
 
-
-# create default vpc if one does not exit
+# create default vpc if one does not exist
 resource "aws_default_vpc" "default_vpc" {
-
-  tags    = {
-    Name  = "Vpc Mahfuz"
+  tags = {
+    Name = "Vpc Mahfuz - Terraform"
   }
 }
 
-
-# use data source to get all avalablility zones in region
+# use data source to get all availability zones in the region
 data "aws_availability_zones" "available_zones" {}
 
-
-# create default subnet if one does not exit
-resource "aws_default_subnet" "default_az1" {
-  availability_zone = "us-east-1a"
-  map_public_ip_on_launch = true
-
-  tags   = {
-    Name = "Subnet Mahfuz"
+# aws_eip
+resource "aws_eip" "nat_eip" {
+  vpc  = true
+  tags = {
+    Name = "EIP Mahfuz"
   }
 }
 
+# nat table
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet.id
+  tags = {
+    Name = "NAT Gateway Mahfuz"
+  }
+}
 
-# create security group for the ec2 instance
+# route gateway
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_default_vpc.default_vpc.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+  tags = {
+    Name = "Route Table Mahfuz"
+  }
+}
+
+# create default subnet if one does not exist
+resource "aws_subnet" "public_subnet" {
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+  vpc_id                  = aws_default_vpc.default_vpc.id
+  cidr_block              = cidrsubnet(aws_default_vpc.default_vpc.cidr_block, 8, 128)
+
+  tags = {
+    Name = "Subnet Mahfuz Public"
+  }
+}
+
+# create security group for the EC2 instance
 resource "aws_security_group" "ec2_security_group" {
   name        = "ec2 security group"
   description = "allow access on ports 80 and 22"
   vpc_id      = aws_default_vpc.default_vpc.id
 
   ingress {
-    description      = "http access"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "http access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    description      = "ssh access"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "ssh access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = -1
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags   = {
+  tags = {
     Name = "Security Group Mahfuz"
   }
 }
 
-
-# launch the ec2 instance and install website
+# launch the EC2 instance and install website
 resource "aws_instance" "ec2_instance" {
-  ami                    = "ami-09f59285244c19131"
-  instance_type          = "t2.micro"
-  subnet_id              = aws_default_subnet.default_az1.id
+  count                 = 2
+  ami                   = "ami-09f59285244c19131"
+  instance_type         = "t2.micro"
+  subnet_id             = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
-  key_name               = "MahfuzKey"
-#  user_data              = 
+  key_name              = "MahfuzKey"
+  user_data             = file("userdata.tpl")
 
   tags = {
-    Name = "Aplicação ELB Mahfuz"
+    Name = "Ec2-${count.index + 1}"
   }
 }
-
-
-# print the ec2's public ipv4 address
-output "public_ipv4_address" {
-  value = aws_instance.ec2_instance.public_ip
-}
-
